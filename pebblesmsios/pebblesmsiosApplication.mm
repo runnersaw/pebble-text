cde001c7102247862afe85869c6fe281270450d0
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2016-01-14 00:02:09 -0700

    Add pebblekit

--- pebblesmsios/pebblesmsiosApplication.mm
+++ pebblesmsios/pebblesmsiosApplication.mm
@@ -1,5 +1,5 @@
 #import "RootViewController.h"
-#import "PebbleKit.h"
+#import <PebbleKit/PebbleKit.h>
 
 @interface pebblesmsiosApplication: UIApplication <UIApplicationDelegate, PBPebbleCentralDelegate> {
 	UIWindow *_window;
