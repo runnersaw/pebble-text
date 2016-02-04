7893527c90624f50392f533b48fa00b60d8d9f3a
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2016-02-03 22:29:25 -0500

    Remove old pebblesmsios

--- pebblesmsios/RootViewController.mm
+++
@@ -1,9 +0,0 @@
-#import "RootViewController.h"
-//#import <PebbleKit/PebbleKit.h>
-
-@implementation RootViewController
-- (void)loadView {
-	self.view = [[[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]] autorelease];
-	self.view.backgroundColor = [UIColor blueColor];
-}
-@end
