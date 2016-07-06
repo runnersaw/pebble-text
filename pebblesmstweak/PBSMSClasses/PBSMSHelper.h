e3435073cad3d2b59937b06f9866aedca12924a9
Sawyer Vaughan <svaughan@onshape.com>
2016-07-06 00:25:25 -0400

    Working on notification actions

--- pebblesmstweak/PBSMSClasses/PBSMSHelper.h
+++ pebblesmstweak/PBSMSClasses/PBSMSHelper.h
@@ -54,6 +54,12 @@ extern NSString *actionsToPerformFileLocation;
 extern NSString *messagesFileLocation;
 extern NSString *recentFileLocation;
 
+#ifdef DEBUG
+	#define log( s, ... ) NSLog( @"<%@:%d> %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__,  [NSString stringWithFormat:(s), ##__VA_ARGS__] )
+#else
+	#define log( s, ... )
+#endif
+
 void dumpMethods(Class clz)
 {
     unsigned int methodCount = 0;
@@ -76,11 +82,9 @@ void dumpMethods(Class clz)
 #ifdef DEBUG
 	#define dumpInstanceMethods( c ) dumpMethods(c)
 	#define dumpClassMethods( c ) dumpMethods(object_getClass(c))
-	#define log( s, ... ) NSLog( @"<%@:%d> %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__,  [NSString stringWithFormat:(s), ##__VA_ARGS__] )
 #else
 	#define dumpInstanceMethods( c )
 	#define dumpClassMethods( c )
-	#define log( s, ... )
 #endif
 
 @interface NSDictionary (PBSMS)
